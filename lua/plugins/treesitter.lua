fe31713d1e1145eb4b293925603d34891afeeff4
nords <nordstrom.rasmus@gmail.com>
2024-01-27 09:47:32 +0100

    Format files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    config = function ()
-        require'nvim-treesitter.install'.compilers = { "gcc" }
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = {"lua", "javascript", "python"},
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.install").compilers = { "gcc" }
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "javascript", "python" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,21 +7,21 @@ vim.wo.number = true -- Show actual line number on current line
 vim.g.mapleader = " "
 
 -- Disable leader key from moving forward in normal mode
-vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", { noremap = true, silent = true })
 
 -- Mappings for SE Keyboard setup
-vim.api.nvim_set_keymap('n', '¤', '$', {noremap = true})
-vim.api.nvim_set_keymap('v', '¤', '$', {noremap = true})
-vim.api.nvim_set_keymap('o', '¤', '$', {noremap = true})
+vim.api.nvim_set_keymap("n", "¤", "$", { noremap = true })
+vim.api.nvim_set_keymap("v", "¤", "$", { noremap = true })
+vim.api.nvim_set_keymap("o", "¤", "$", { noremap = true })
 
-vim.api.nvim_set_keymap('i', 'å', '[', {noremap = true})
-vim.api.nvim_set_keymap('o', 'å', '[', {noremap = true})
-vim.api.nvim_set_keymap('i', '¨', ']', {noremap = true})
-vim.api.nvim_set_keymap('o', '¨', ']', {noremap = true})
+vim.api.nvim_set_keymap("i", "å", "[", { noremap = true })
+vim.api.nvim_set_keymap("o", "å", "[", { noremap = true })
+vim.api.nvim_set_keymap("i", "¨", "]", { noremap = true })
+vim.api.nvim_set_keymap("o", "¨", "]", { noremap = true })
 
-vim.api.nvim_set_keymap('i', 'Å', '{', {noremap = true})
-vim.api.nvim_set_keymap('o', 'Å', '{', {noremap = true})
-vim.api.nvim_set_keymap('i', '^', '}', {noremap = true})
-vim.api.nvim_set_keymap('o', '^', '}', {noremap = true})
+vim.api.nvim_set_keymap("i", "Å", "{", { noremap = true })
+vim.api.nvim_set_keymap("o", "Å", "{", { noremap = true })
+vim.api.nvim_set_keymap("i", "^", "}", { noremap = true })
+vim.api.nvim_set_keymap("o", "^", "}", { noremap = true })
 
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
