b6d66c79116707c1910cc9edb79837e517a2ad7b
nords <nordstrom.rasmus@gmail.com>
2024-01-27 11:08:43 +0100

    Map live grep to <Leader>o

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<C-o>", builtin.live_grep, {})
+			vim.keymap.set("n", "<Leader>o", builtin.live_grep, {})
 		end,
 	},
 	{
