065ee43d0a84098e0c3bb72100ff2a38ee437ed5
nords <nordstrom.rasmus@gmail.com>
2024-01-28 17:37:37 +0100

    Fix search highlight and bindings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,14 +3,20 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.wo.relativenumber = true -- Relative numbers
-vim.wo.number = true         -- Show actual line number on current line
+vim.wo.number = true -- Show actual line number on current line
 vim.g.mapleader = " "
 
 -- Disable leader key from moving forward in normal mode
 vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", { noremap = true, silent = true })
 
+-- Makes the search light dissapear as soon as we press enter
+vim.api.nvim_set_keymap('c', '<CR>', '<CR>:nohlsearch<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<CR>', ':nohlsearch<CR>', { noremap = true, silent = true })
+
 -- Mappings for SE Keyboard setup
 vim.keymap.set({ "n", "v", "o" }, "¤", "$", { noremap = true })
+vim.keymap.set({ "n", "v", "o" }, "-", "/", { noremap = true })
+vim.keymap.set({ "n", "v", "o" }, "_", "?", { noremap = true })
 vim.keymap.set({ "i", "o" }, "å", "[", { noremap = true })
 vim.keymap.set({ "i", "o" }, "¨", "]", { noremap = true })
 vim.keymap.set({ "i", "o" }, "Å", "{", { noremap = true })
@@ -33,3 +39,4 @@ vim.keymap.set({ "n", "o" }, "få", "f[", { noremap = true })
 
 -- Makes it possible to escape terminal with <Esc>
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+
