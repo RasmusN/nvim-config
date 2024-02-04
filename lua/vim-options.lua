b745cd9004c32e8e2c1129f3bb04975fc3cc663d
nords <nordstrom.rasmus@gmail.com>
2024-02-04 19:04:56 +0100

    Add file autorefresh on external change

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,15 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- Auto refresh externally changed files 
+-- https://stackoverflow.com/questions/62100785/auto-reload-file-and-in-neovim-and-auto-reload-nerbtree
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
+-- Autorun git bash terminal if on windows
 if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.cmd [[let &shell = '"C:\Program Files\Git\bin\bash.exe"']]
     vim.cmd [[let &shellcmdflag = '-s']]
