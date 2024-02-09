4fc01528fa43e642d497257f7e8ea99fcadf2188
nords <nordstrom.rasmus@gmail.com>
2024-02-09 22:18:57 +0100

    Set lualine winbar file name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,22 @@ return {
         require('lualine').setup({
             options = {
                 theme = 'dracula'
+            },
+            winbar = {
+                  lualine_a = {{'filename', path=1}},
+                  lualine_b = {},
+                  lualine_c = {},
+                  lualine_x = {},
+                  lualine_y = {},
+                  lualine_z = {}
+            },
+            inactive_winbar  = {
+                  lualine_a = {},
+                  lualine_b = {{'filename', path=1}},
+                  lualine_c = {},
+                  lualine_x = {},
+                  lualine_y = {},
+                  lualine_z = {}
             }
         })
     end
