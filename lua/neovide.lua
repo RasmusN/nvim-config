0164b62fc73614d9a8d1c4a96a8e1010c1fc53be
nords <nordstrom.rasmus@gmail.com>
2024-02-09 22:41:29 +0100

    Make everything bigger

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,5 +1,5 @@
 -- Configurations for the Neovim GUI Neovide
 
-vim.g.neovide_scale_factor = 0.7
+vim.g.neovide_scale_factor = 1.0
 vim.g.neovide_scroll_animation_length = 0.1
 vim.g.neovide_cursor_animation_length = 0.03
