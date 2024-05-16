6abbf7bcf38a855eab084275b1f571d0c174a3d6
Rasmus Nordström <rasmus.nordstrom@lotsgroup.com>
2024-05-16 11:25:33 +0200

    bind <leader>gd to :Gitsigns diffthis

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,6 +4,7 @@ return {
         require("gitsigns").setup()
         vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
         vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+        vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", {})
     end
 }
 
