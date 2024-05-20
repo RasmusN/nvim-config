00f8bd730283f2885367bf071c1bc405464dd6a7
Rasmus Nordström <rasmus.nordstrom@lotsgroup.com>
2024-05-20 14:59:28 +0200

    bind <leader>ga to git stage current buffer

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,6 +5,7 @@ return {
         vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
         vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
         vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", {})
+        vim.keymap.set("n", "<leader>ga", ":Gitsigns stage_buffer<CR>", {})
     end
 }
 
