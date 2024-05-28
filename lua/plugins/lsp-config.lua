8fb192796c0b3ef8fb9cbe5a78332781a850f9cd
Rasmus Nordström <rasmus.nordstrom@lotsgroup.com>
2024-05-28 07:51:56 +0200

    fix bug making pyright not find root dir

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -48,9 +48,7 @@ return {
 					},
 				},
 			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+			lspconfig.pyright.setup({})
 			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
