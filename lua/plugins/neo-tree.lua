4fca10637651cd81d24adb985a92a45aefc52e43
Rasmus Nordström <rasmus.nordstrom@lotsgroup.com>
2024-06-02 10:25:20 +0200

    Enable normal mode for neotree.
    This doesn't work though but I'll keep it on if it happens to be a bug that will be fixed in the future.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,7 +9,7 @@ return {
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
 		require("neo-tree").setup({
-			enable_normal_mode_for_inputs = false,
+			enable_normal_mode_for_inputs = true,
 			window = {
 				position = "left",
 				width = 40,
