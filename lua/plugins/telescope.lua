4837c6b37b4629f5be7a709fbf2abec386b696d6
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2025-08-21 09:33:49 +0200

    make <leader>pf search fixed strings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,9 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<Leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<Leader>pf", function()
+				builtin.live_grep({ additional_args = { "--fixed-strings" } })
+			end, {})
 			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 			vim.keymap.set("n", "<Leader>ps", function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") })
