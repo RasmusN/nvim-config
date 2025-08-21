1ace4d4e3377c3e1329c0775122d9506704a2621
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2025-08-21 09:43:08 +0200

    set ff=find files, fs=file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,8 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<Leader>pf", function()
+            vim.keymap.set("n", "ff", builtin.find_files, {})
+            vim.keymap.set("n", "fs", function()
 				builtin.live_grep({ additional_args = { "--fixed-strings" } })
 			end, {})
 			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
