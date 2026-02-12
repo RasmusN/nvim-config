4983e5e3eebe9d2ad32fc06676643edf2f5d8742
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2026-02-12 15:32:32 +0100

    Add numbered terminal keybindings
    Bind <leader>1/2/3 to toggle individual float
    terminals. Also fix indentation (spaces to tabs)
    in toggleterm config.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,6 +3,11 @@ return {
 	version = "*",
 	opts = {
 		open_mapping = "<C-.>",
-        direction = 'float',
+		direction = "float",
+	},
+	keys = {
+		{ "<leader>1", "<Cmd>1ToggleTerm direction=float<CR>", mode = { "n", "t" }, desc = "Toggle terminal 1" },
+		{ "<leader>2", "<Cmd>2ToggleTerm direction=float<CR>", mode = { "n", "t" }, desc = "Toggle terminal 2" },
+		{ "<leader>3", "<Cmd>3ToggleTerm direction=float<CR>", mode = { "n", "t" }, desc = "Toggle terminal 3" },
 	},
 }
